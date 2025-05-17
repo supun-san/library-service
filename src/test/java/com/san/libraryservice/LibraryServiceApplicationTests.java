995bfb5f87d75b55c78bd342dcba804307292a8d
Supunsan <supun-san@users.noreply.github.com>
2025-05-17 09:19:15 +0530

    Initial commit

---
+++ src/test/java/com/san/libraryservice/LibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.san.libraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
